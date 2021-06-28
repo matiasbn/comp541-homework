5f3893d7c23e2616b98e421bc11a0e7679973d22
Matías Barrios <matias.barriosn@gmail.com>
2021-06-28 18:35:33 -0400

    feat: public key from command line

--- first-program.py
+++ first-program.py
@@ -1,6 +1,6 @@
 from bitcoinutils.setup import setup
 from bitcoinutils.transactions import Transaction, TxInput, TxOutput, Sequence
-from bitcoinutils.keys import P2pkhAddress, P2shAddress, PrivateKey
+from bitcoinutils.keys import P2pkhAddress, P2shAddress, PrivateKey, PublicKey
 from bitcoinutils.script import Script
 from bitcoinutils.constants import TYPE_RELATIVE_TIMELOCK
 import sys
@@ -9,20 +9,19 @@ import sys
 def main():
     setup('testnet')
 
-    relative_blocks = 20
+    # parameter 1, block height
+    relative_blocks = int(sys.argv[1])
     seq = Sequence(TYPE_RELATIVE_TIMELOCK, relative_blocks)
-
     # p2pkh_sk = PrivateKey(
-    #     'cRvyLwCPLU88jsyj94L7iJjQX5C2f8koG4G2gevN4BeSGcEvfKe9')
-
-    # p2pkh_addr = p2pkh_sk.get_public_key().get_address()
-    # get address from command line
-    p2pkh_addr = sys.argv[1]
+    #     '9269dzjLf6SVfNtiQXvcszv44rLY4ZQV24dvAikhTJmQqQpfsao')
+    # p2pkh_addr = p2pkh_sk.get_public_key()
+    p2pkh_addr = (PublicKey(sys.argv[2]))
     redeem_script = Script([seq.for_script(), 'OP_CHECKSEQUENCEVERIFY', 'OP_DROP',
-                           'OP_DUP', 'OP_HASH160', p2pkh_addr, 'OP_EQUALVERIFY', 'OP_CHECKSIG'])
+                           'OP_DUP', 'OP_HASH160', p2pkh_addr.get_address().to_hash160(), 'OP_EQUALVERIFY', 'OP_CHECKSIG'])
 
     addr = P2shAddress.from_script(redeem_script)
-    print(f"block before unlock: {relative_blocks}")
+    print(f"public address: {p2pkh_addr.get_address().to_hash160()}")
+    print(f"block height: {relative_blocks}")
     print(f"p2sh address: {addr.to_string()}")
 
 
